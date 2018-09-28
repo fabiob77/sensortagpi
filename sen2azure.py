7ca173140733edd9ea3888cbbc7e85d1aaf00a02
Fabio Bonisoli <faboniso@microsoft.com>
2018-09-28 18:45:49 +0100

    dd

--- sen2azure.py
+++ sen2azure.py
@@ -481,6 +481,58 @@ def main():
     if (arg.light or arg.all) and tag.lightmeter is not None:
         tag.lightmeter.enable()
 
+# Define the JSON message to send to IoT Hub.
+if arg.temperature or arg.all:
+           TEMPERATURE =  tag.IRtemperature.read()
+       if arg.humidity or arg.all:
+           HUMIDITY = tag.humidity.read()
+MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
+        def send_confirmation_callback(message, result, user_context):
+    print ( "IoT Hub responded to message with status: %s" % (result) )
+
+def iothub_client_init():
+    # Create an IoT Hub client
+   # client.set_option("auto_url_encode_decode", True)
+    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        while True:
+            # Build the message with simulated telemetry values.
+            temperature = TEMPERATURE + (random.random() * 15)
+            humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT % (temperature, humidity)
+            message = IoTHubMessage(msg_txt_formatted)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            prop_map = message.properties()
+            if temperature > 30:
+              prop_map.add("temperatureAlert", "true")
+            else:
+              prop_map.add("temperatureAlert", "false")
+
+            # Send the message.
+            print( "Sending message: %s" % message.get_string() )
+            client.send_event_async(message, send_confirmation_callback, None)
+            time.sleep(1)
+
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+if __name__ == '__main__':
+    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_sample_run()
+
     # Some sensors (e.g., temperature, accelerometer) need some time for initialization.
     # Not waiting here after enabling a sensor, the first read value might be empty or incorrect.
     time.sleep(1.0)
