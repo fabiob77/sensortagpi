c923686f8b6cd545991ef475fedde7d3809da2a9
Fabio Bonisoli <faboniso@microsoft.com>
2018-09-29 19:17:52 +0100

    fbdraft

--- sen2azure.1.py
+++ sen2azure.1.py
@@ -489,18 +489,23 @@ def main():
        if arg.humidity or arg.all:
            print("Humidity: ", tag.humidity.read())
            HUMIDITY = tag.humidity.read()
+           
+       if counter >= arg.count and arg.count != 0:
+           break
+       counter += 1
+       tag.waitForNotifications(arg.t) 
            # Define the JSON message to send to IoT Hub.
            #MSG_TXT = "{\"temperature\": 'TEMPERATURE',\"humidity\": 'HUMIDITY'}"
-           def send_confirmation_callback(message, result, user_context):
-            print ( "IoT Hub responded to message with status: %s" % (result) )
+           #def send_confirmation_callback(message, result, user_context):
+           # print ( "IoT Hub responded to message with status: %s" % (result) )
            
-           def iothub_client_init():
+    def iothub_client_init():
             # Create an IoT Hub client
             # client.set_option("auto_url_encode_decode", True)
             client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
             return client
            
-           def iothub_client_telemetry_sample_run():
+    def iothub_client_telemetry_sample_run():
             try:
               client = iothub_client_init()
               print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
@@ -526,12 +531,8 @@ def main():
                 return
             except KeyboardInterrupt:
                 print ( "IoTHubClient sample stopped" )
-
-       if counter >= arg.count and arg.count != 0:
-           break
-       counter += 1
-       tag.waitForNotifications(arg.t)       
-       if __name__ == "__main__":
+      
+    if __name__ == "__main__":
             main()
             if __name__ == '__main__':
               print ( "IoT Hub Quickstart #1 - R device" )
